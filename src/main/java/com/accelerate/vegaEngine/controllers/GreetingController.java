9d36564068fb790936bb6d1355a56053e0344f67
Abhijith C S <abhijithcs1993@gmail.com>
2018-09-29 22:01:09 +0530

    Basic Structure - added

--- src/main/java/com/accelerate/vegaEngine/controllers/GreetingController.java
+++ src/main/java/com/accelerate/vegaEngine/controllers/GreetingController.java
@@ -1,10 +1,11 @@
 package com.accelerate.vegaEngine.controllers;
 
 import java.util.concurrent.atomic.AtomicLong;
-
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.accelerate.vegaEngine.models.*;
 
 @RestController
 public class GreetingController {
@@ -14,6 +15,7 @@ public class GreetingController {
 
     @RequestMapping("/greeting")
     public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
+    	    	
         return new Greeting(counter.incrementAndGet(),
                             String.format(template, name));
     }
